60323cb79a9a83f80d1904c0056fa782398701c1
C0D8 <matheusaguiar362@gmail.com>
2023-05-02 10:59:45 -0300

    Lógica de páginação funcionando

--- FrontBike/src/App.jsx
+++ FrontBike/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import {Link} from 'react-router-dom'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,6 +23,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+
+        <Link to='/' ><button color="inherit">Home </button></Link>
+        <Link to='/hello'><button color="inherit">Hello</button></Link>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- FrontBike/src/main.jsx
+++ FrontBike/src/main.jsx
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/hello",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
