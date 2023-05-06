8202e1180e5dc97d7178009e2c3c9e50f660b395
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2023-05-06 18:13:22 -0300

    app.jsx melhorado e com css ajustado

--- FrontBike/src/App.jsx
+++ FrontBike/src/App.jsx
@@ -4,36 +4,41 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import {Link} from 'react-router-dom'
+import { NavBar } from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <NavBar/>
+      <div className="page">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
 
-        <Link to='/' ><button color="inherit">Home </button></Link>
-        <Link to='/new' ><button color="inherit">Create Bike </button></Link>
-        <Link to='/hello'><button color="inherit">Hello</button></Link>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <Link to='/' ><button color="inherit">Home </button></Link>
+          <Link to='/new' ><button color="inherit">Create Bike </button></Link>
+          <Link to='/bike' ><button color="inherit">List Bikes </button></Link>
+          <Link to='/hello'><button color="inherit">Hello</button></Link>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
